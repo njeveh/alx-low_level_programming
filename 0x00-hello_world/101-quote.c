e257e27c8307a8b2deb2ab33bfb3ad14d9582d07
njeveh <njeveh@gmail.com>
2022-03-10 08:47:03 -0800

    print a string to std out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Main is the entry to the program
- * it returns 1 upon success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
