8b84e8528c7f0f64500ca63817f571aaa10fb88c
njeveh <njeveh@gmail.com>
2022-03-15 07:49:28 -0700

    print exactly _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print triple combos
+ *
+ * Description: print triple digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
