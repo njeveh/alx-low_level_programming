9df13293c33afe2b6d885cc0bfbe2f22b4ef2302
njeveh <njeveh@gmail.com>
2022-03-15 10:01:29 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+nclude "main.h"
 
 /**
- * main - print triple combos
+ * main - prints Holberton
  *
- * Description: print triple digit combos
+ * Description: prints _putchar
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char text[7] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
