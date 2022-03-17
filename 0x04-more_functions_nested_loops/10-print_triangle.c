359445c7967faf678104bcddf8b7a844ee4fcb31
njeveh <njeveh@gmail.com>
2022-03-17 09:24:14 -0700

    corrections

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; ++i)
+		for (i = size - 1; i >= 0; --i)
 		{
-			for (j = 0; j <= i ; ++j)
+			for (j = 0; j < size; ++j)
 			{
 				if (j < i)
 				{
