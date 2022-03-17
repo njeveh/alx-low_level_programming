8e1ebbb6c1ab78cd3cfc415d67cff38bbdcdd1a4
njeveh <njeveh@gmail.com>
2022-03-17 06:35:51 -0700

    add a function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,17 +17,12 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; ++i)
 		{
-
 			for (j = 0; j < size ; ++j)
 			{
-				if (i == 0 || i == (size - 1) || j == 0 || j == (size - 1))
-				{
-					_putchar('#');
-				}
-				else
-				{
-					_putchar(' ');
-				}
+				_putchar('#');
+
 			}
 			_putchar(10);
 		}
+	}
+}
