adc8cac523d45d369bd3c074e52d44f0dc9f3b01
njeveh <njeveh@gmail.com>
2022-04-01 17:45:38 +0300

    add a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the result of the multiplication of its arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0) on success otherwise 1
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
