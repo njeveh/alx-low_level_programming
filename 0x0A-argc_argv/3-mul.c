fd563922b87846ff9caaa3779243bcbf0bae934d
njeveh <njeveh@gmail.com>
2022-04-01 17:51:50 +0300

    add a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,14 @@
 
 int main(int argc, char *argv[])
 {
+	int mul;
+
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", argv[1] * argv[2]);
+	mul = (int) argv[1] * argv[2];
+	printf("%d\n", mul);
 	return (0);
 }
