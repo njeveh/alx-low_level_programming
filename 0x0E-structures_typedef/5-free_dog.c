357ccdf826d8b58c5b8c7f33f998b14e78806eac
njeveh <njeveh@gmail.com>
2022-04-12 00:30:43 +0300

    add a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees struct dog
+ * @d: pointer to struct dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
