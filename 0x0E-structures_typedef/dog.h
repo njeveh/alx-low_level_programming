4653472e22fc8469bc3a1072ce8eaaa80375dfa8
njeveh <njeveh@gmail.com>
2022-04-11 23:34:42 +0300

    add header files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,29 @@
 #ifndef DOG_H
 #define DOG_H
-
-int _putchar(char c);
-
 /**
- * struct dog - a dog's representation
- * @name: the dog's name
- * @age: the dog's age
- * @owner: The dog's owner
+ * struct dog - Characteristics of a dog
+ * @name: The dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
  *
- * Description: a structure containimg a dog's data
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+  * dog_t - Typedef for struct dog
+  */
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 
 #endif /* DOG_H */
