e9cd3fc9df19850ee0888dbef99fd765f629b584
njeveh <njeveh@gmail.com>
2022-04-12 00:13:24 +0300

    add a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,53 +2,73 @@
 #include <stlib.h>
 
 /**
- * *new_dog -  creates a new dog
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer to newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	int length, i;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	arr = (char *) malloc(length * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+	arr[i] = '\0';
+
+	return (arr);
+}
+
+/**
+ * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
- * Return: pointer to new dog on success, else NULL
+ * Return: new_dog (dog_t)
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, len_name, len_owner;
+	dog_t *dog;
+	char *dog_name, *dog_owner;
 
-	dog_t *new_dog = NULL;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-	len_name = 0, len_owner = 0;
-	while (name[len_name] != '\0')
-		len_name++;
-	while (owner[len_owner] != '\0')
-		len_owner++;
-
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
+	dog_name = _strdup(name);
+	if (dog_name == NULL)
 	{
-		free(new_dog);
+		free(dog);
 		return (NULL);
 	}
 
-	new_dog->name = malloc(len_name + 1);
-	if (new_dog->name == NULL)
+	dog_owner = _strdup(owner);
+	if (dog_owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(dog_name);
+		free(dog);
 		return (NULL);
 	}
-
-	new_dog->owner = malloc(len_owner + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
-		return (NULL);
-	}
-
-	for (i = 0; i <= len_name; i++)
-		new_dog->name[i] = name[i];
-	for (i = 0; i <= len_owner; i++)
-		new_dog->owner[i] = owner[i];
-	new_dog->age = age;
-	return (new_dog);
+	dog->name = dog_name;
+	dog->age = age;
+	dog->owner = dog_owner;
+	return (dog);
 }
