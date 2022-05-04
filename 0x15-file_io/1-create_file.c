43fe7dbf77bdb188ab3a1ae9f8d82daa5166d329
njeveh <njeveh@gmail.com>
2022-05-04 14:31:40 +0300

    add file io functions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,54 @@
 #include "main.h"
 
 /**
-  * create_file - Creates a file
-  * @filename: Name of file to create
-  * @text_content: String  to write to file
-  *
-  * Return: 1 on success -1 on failure
-  */
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text
+ * Return: 1 - success, -1 error
+ */
 
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t len = 0;
-	ssize_t check = 0;
+	unsigned int length;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
-
-	/*Find length of content*/
-	while (text_content[len] && text_content)
-		len++;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		check = write(fd, text_content, len);
 
-	if (check == -1)
+	if (text_content == NULL)
+	{
+		length = 0;
+		text_content = "";
+	}
+	else
+		length = _strlen(text_content);
+
+	bytes_written = write(fd, text_content, length);
+	if (bytes_written == -1)
 		return (-1);
-
 	close(fd);
 	return (1);
 }
