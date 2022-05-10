0179879b793e7879193a104f9c4e8af2e9de1e0e
njeveh <njeveh@gmail.com>
2022-05-10 12:23:38 +0300

    add a function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,33 +10,31 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
-	va_list ap;
-	char *str;
+	char *string;
 
 	if (n == 0)
 	{
 		printf("\n");
 		return;
 	}
-
-	va_start(ap, n);
-	for (i = 0; i < n; ++i)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ap, char *);
-		str = str != NULL ? str : "(nil)";
-		if (separator == NULL)
+		string = va_arg(args, char *);
+		if (string == NULL)
+			string = "(nil)";
+
+		if (i != n - 1)
 		{
-			if (i == n - 1)
-				printf("%s\n", str);
-			printf("%s", str);
+			if (separator == NULL)
+				printf("%s", string);
+			else
+				printf("%s%s", string, separator);
 		}
 		else
-		{
-			if (i == n - 1)
-				printf("%s\n", str);
-			printf("%s%s", str, separator);
-		}
+			printf("%s\n", string);
 	}
-	va_end(ap);
+	va_end(args)
 }
